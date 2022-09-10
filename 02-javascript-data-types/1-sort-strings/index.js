bbd760551d248abc3222a88a52396b6be026c6eb
looo89 <looo89@yandex.ru>
2022-09-10 15:01:42 +0300

    sortString function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,13 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    const newStrings = [...arr];
 
+    const compareString = (str1, str2) => {
+        return str1.localeCompare(str2, [], { caseFirst: "upper" });
+    };
+
+    return newStrings.sort((str1, str2) =>
+        param === "asc" ? compareString(str1, str2) : compareString(str2, str1)
+    ); 
 }
