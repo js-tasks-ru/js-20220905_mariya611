9155c37551f6b807b4e95803cd9bb82aaaf29025
looo89 <looo89@yandex.ru>
2022-09-14 11:40:05 +0300

    trimSymbol function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,31 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if(size === 0){
+        return ''
+    }
+    if( !size){
+        return string
+    }
 
+    let stringArr = string.split('')
+    let newStringArr =[]
+    let currentSymbol = ''
+    let currentNuber=1  // 
+    for (let i = 0; i < stringArr.length; i++) {
+        if(stringArr[i] === currentSymbol){
+            if(currentNuber===size){
+                continue;
+            } else{
+                newStringArr.push(stringArr[i])
+                currentNuber +=1
+            }
+        } else {
+            newStringArr.push(stringArr[i])
+            currentSymbol = stringArr[i]
+            currentNuber=1
+        }
+    }
+
+    return newStringArr.join('')
 }
