93aeaf2752fbbc2377b601a0ba1ababb17b7c199
looo89 <looo89@yandex.ru>
2022-09-14 09:59:56 +0300

    createGetter function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const filds = path.split(".");                        //array
 
+    return (obj) => {
+        let copyObj = obj;
+        for (const iterator of filds) {
+            if (typeof copyObj[iterator] === "object") {
+                copyObj = copyObj[iterator];
+              } else {
+                return copyObj[iterator]
+              }
+        }
+    };
 }
